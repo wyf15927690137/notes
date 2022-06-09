c44717d28eb44c1a4bfbe572e4fda3e4d0399a2f
yanfeiw <weiyanfei@sjtu.edu.cn>
2022-06-09 22:40:37 +0800

    local

---
+++ GitShortCutsSet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git config --global alias.st status
+
+git config --global alias.ph push
+
+git config --global alias.df diff
+
+git config --global alias.cm commit
+
+git config --global alias.ad add
+
+
+
